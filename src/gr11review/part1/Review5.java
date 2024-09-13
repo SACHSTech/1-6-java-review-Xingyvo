5c5ff5096cbae89cb444022966d056d8c214abb7
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-09-12 21:55:19 -0400

    Worked on review 5 and 6.

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,44 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        // Declare and intialize variables
+        double dblAmount;
+        double dblYearlyAmount;
+        double dblRate;
+        double dblCompoundInterestRate;
+        double dblTargetAmount;
+
+        // Ask user for invested amount, compound interest rate, and target amount
+        System.out.print("Enter the yearly invested amount: ");
+        dblAmount = Double.parseDouble(keyboard.readLine());
+
+        System.out.print("Enter the compound interest rate: ");
+        dblCompoundInterestRate = Double.parseDouble(keyboard.readLine());
+
+        System.out.print("Enter the target amount: ");
+        dblTargetAmount = Double.parseDouble(keyboard.readLine());
+
+        // Initialize variable
+        dblYearlyAmount = dblAmount;
+        double dblYear = 0;
+
+        // Calculate how long it will take to earn target amount
+        while (dblAmount <= dblTargetAmount) {
+            // Calculate interest rate and total 
+            dblRate = dblCompoundInterestRate / dblYearlyAmount * dblAmount;
+            dblAmount = dblAmount + dblRate;
+
+            // Update the year and add the yearly invested amount
+            dblYear++;
+            dblAmount += dblYearlyAmount;
+        }
+
+        // Output result
+        System.out.println("The target amount will be earned in " + dblYear + " years.");
+    }
+}
--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,1 +1,42 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.text.*;
+
+public class Review6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        NumberFormat numberFormat = new DecimalFormat("$#,###.00"); 
+
+        // Declare and initialize variables
+        double dblPrice = 0;
+        double dblTax;
+        double dblSubtotal = 0;
+        double dblTotal = 0;
+        boolean blnRun = true;
+
+        // Ask for the price of an item, stops when user enters 0
+        while (blnRun) {
+            System.out.print("Enter the price for an item: ");
+            dblPrice = Double.parseDouble(keyboard.readLine());
+
+            // Update subtotal
+            dblSubtotal += dblPrice;
+
+            if (dblPrice == 0) {
+                blnRun = false;
+            }
+        }
+
+        // Output subtotal
+        System.out.println("Subtotal: " + numberFormat.format(dblSubtotal));
+
+        // Calculate and output tax
+        dblTax = dblSubtotal * 0.13;
+        System.out.println("Tax: " + numberFormat.format(dblTax));
+
+        // Calculate and output total
+        dblTotal = dblSubtotal + dblTax;
+        System.out.println("Total: " + numberFormat.format(dblTotal));
+    }
+}
