8c4738a31a89148ed70929c1d36add6cbb13fcb9
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-09-12 20:47:43 -0400

    Worked on review 3 and 4.

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,19 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        
+        // Prints the odd numbers from 20 to 100
+        for (int i = 21; i <= 100; i+=2) {
+            System.out.println(i);
+        }
+
+        // Prints the numbers from 29 to 2 in decreasing order
+        for (int i = 29; i >= 2; i--) {
+            System.out.println(i);
+        }
+    }
+}
--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,42 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.text.*;
+
+public class Review4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        NumberFormat numberFormat = new DecimalFormat("$#,###.00"); 
+    
+        // Declare and intialize variables
+        int intNumOfItems; 
+        double dblPrice;
+        double dblTax;
+        double dblSubtotal = 0;
+        double dblTotal = 0;
+
+        // Ask for how many items
+        System.out.print("How many items do you want to buy? ");
+        intNumOfItems = Integer.parseInt(keyboard.readLine());
+
+        // Ask user for price of each item
+        for (int i = 1; i <= intNumOfItems; i++) {
+            System.out.print("Enter the price for item " + i + ": ");
+            dblPrice = Double.parseDouble(keyboard.readLine());
+
+            // Calculate subtotal
+            dblSubtotal += dblPrice;
+        }
+
+        // Output subtotal
+        System.out.println("Subtotal: " + numberFormat.format(dblSubtotal));
+
+        // Calculate tax and output tax
+        dblTax = dblSubtotal * 0.13;
+        System.out.println("Tax: " + numberFormat.format(dblTax));
+
+        // Calculate and output total
+        dblTotal = dblSubtotal + dblTax;
+        System.out.println("Total: " + numberFormat.format(dblTotal));
+    }
+}
