e0eeee3e1f869c9951b3080b5f5faded9dd94e3e
Xingyvo <159099828+Xingyvo@users.noreply.github.com>
2024-09-16 14:46:37 -0400

    Added a javadoc to review 3.

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,8 +1,19 @@
 package gr11review.part1;
 
 import java.io.*;
-
+ 
+/**
+ * Review 3 prints the odd numbers from 20-100 and prints the numbers from 29-2
+ * in decreaing order with separate loops.
+ */
 public class Review3 {
+
+    /**
+     * Main method to execute the program's logic
+     * 
+     * @param args Command-line arguments (not used)
+     * @throws IOException If there is an error during input
+     */
     public static void main(String[] args) throws IOException {
         
         // Prints the odd numbers from 20 to 100
